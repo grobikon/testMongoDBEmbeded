a9723a755a4063010369d58f73b17a2c8ae0f0be
Образцов Григорий <grobikon@mail.ru>
2021-12-04 23:21:52 +0300

    Add test product MongoDb.

---
+++ src/test/kotlin/ru/grobikon/testmongodbembeded/DevApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.grobikon.testmongodbembeded
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DevApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
